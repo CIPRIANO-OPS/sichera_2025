301083ccbf9b13893355aaf7d5925a199460423e
CIPRIANO-OPS <cipriano.marmolejo.27@unsch.edu.pe>
2025-08-27 09:27:47 -0500

    este es tu comentario

--- app/Models/Cliente.php
+++ app/Models/Cliente.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 //use Illuminate\Database\Eloquent\Factories\HasFactory;
 //use Illuminate\Database\Eloquent\SoftDeletes;
+//use Illuminate\Notifications\Notifiable;
+//use Laravel\Sanctum\HasApiTokens;
 class Cliente extends Model
 {
     protected $fillable = [
